901f25924a600573047fd41196130a02857bf435
Nick Lewis <nickeblewis@gmail.com>
2015-06-18 15:57:15 +0100

    Various updates

--- client/shared/helpers.js
+++ client/shared/helpers.js
@@ -1,3 +1,4 @@
+CalEvent = new Mongo.Collection('calevent');
 Template.registerHelper('Config', function() {
   return Config;
 });
@@ -17,12 +18,74 @@ Template.registerHelper('Utils', function() {
 });
 
 Template.calendar.helpers({
+	calendarOptions: {
+		// Standard fullcalendar options
+		height: 700,
+		hiddenDays: [ 0 ],
+		slotDuration: '01:00:00',
+		minTime: '08:00:00',
+		maxTime: '19:00:00',
+		lang: 'fr',
+		// Function providing events reactive computation for fullcalendar plugin
+		events: function(start, end, timezone, callback) {
+			//console.log(start);
+			//console.log(end);
+			//console.log(timezone);
+			var events = [];
+			// Get only events from one document of the Calendars collection
+			// events is a field of the Calendars collection document
+			var calendar = Calendars.findOne(
+				{ "_id":"myCalendarId" },
+				{ "fields": { 'events': 1 } }
+			);
+			// events need to be an array of subDocuments:
+			// each event field named as fullcalendar Event Object property is automatically used by fullcalendar
+			if (calendar && calendar.events) {
+				calendar.events.forEach(function (event) {
+					eventDetails = {};
+					for(key in event)
+						eventDetails[key] = event[key];
+					events.push(eventDetails);
+				});
+			}
+			callback(events);
+		},
+		// Optional: id of the calendar
+		id: "calendar1",
+		// Optional: Additional classes to apply to the calendar
+		addedClasses: "col-md-8",
+		// Optional: Additional functions to apply after each reactive events computation
+		autoruns: [
+			function () {
+				console.log("user defined autorun function executed!");
+			}
+		]
+	},
     options: function() {
       return {
         header: {
           left: 'prev,next today',
           center: 'title',
           right: 'month,agendaWeek,agendaDay'
+        },
+        dayClick:function(date,allDay,jsEvent,view){
+          var calendarEvent = {};
+          calendarEvent.start = date;
+          calendarEvent.end = date;
+          calendarEvent.title = 'New Booking';
+          calendarEvent.owner = Meteor.userId();
+          Meteor.call('saveCalEvent',calendarEvent);
+        },
+        eventClick:function(calEvent,jsEvent,view){
+          Session.set('editing_event',calEvent._id);
+          $('#title').val(calEvent.title);
+        },
+        eventDrop:function(reqEvent){
+          Meteor.call('moveEvent',reqEvent);
+        },
+        events:function(start,end,callback){
+          var calEvents = CalEvent.find({},{reactive:false}).fetch();
+          callback(calEvents);
         },
         defaultDate: '2015-05-12',
         timezone: 'Europe/London',
